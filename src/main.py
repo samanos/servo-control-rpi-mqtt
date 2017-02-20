4b851b75c3fa637d8ef032445323d3769dd7ed4e
Martynas Mickevičius <martynas@2m.lt>
2017-02-21 01:47:16 +0200

    flex

--- src/main.py
+++ src/main.py
@@ -1,4 +1,30 @@
 from w1thermsensor import W1ThermSensor
 
-for sensor in W1ThermSensor.get_available_sensors():
-    print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+import time
+import random
+
+from flexx import app, ui, event
+
+
+class Monitor(ui.Widget):
+
+    def init(self):
+        with ui.HBox():
+            with ui.VBox():
+                self.info = ui.Label(text='...')
+                ui.Widget(flex=1)
+
+        self.refresh()
+
+    def refresh(self):
+        for sensor in W1ThermSensor.get_available_sensors():
+            self.info.text = "Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature())
+            print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+
+        app.call_later(1, self.refresh)
+
+
+if __name__ == '__main__':
+    app.serve(Monitor)
+    # m = app.launch(Monitor)  # for use during development
+    app.start()
